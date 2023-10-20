ebcebab2b76aa23db823025438dedc6e3a728d58
Laura Buide <laurabuide2@gmail.com>
2023-10-20 21:51:31 +0200

    quarta càrrega

--- practica1.sh
+++ practica1.sh
@@ -20,3 +20,31 @@ while [ "$opcio" != "q" ] ; do
 		  "q") echo "Sortint de l'aplicació"
 		  exit 0
 		  ;;
+		   #2. Llista països
+		   "lp") awk -F',' '!seen[$8]++ {print $7, $8}' cities.csv
+		   ;;
+		    #3. Seleccionar país
+		    "sc") read -p "Selecciona un país: " sc
+		    codi_pais=$(grep "$sc" cities.csv | awk -F ',' -v sc="$sc" '$8 == sc {print $7; exit}')
+		    if [ -n "$codi_pais" ]; then
+			    codi_pais="$codi_pais"
+		    else
+			    codi_pais="XX"
+		    fi
+		    echo "$codi_pais"
+		    ;;
+		    #4. Seleccionar un estat                                                                                
+		    "se") read -p "Selecciona un estat: " se
+		    if [ -z "$sc" ]; then
+			    read -p "Selecciona un país: " sc
+		    fi
+		    if grep -q "$sc" cities.csv; then
+			    if grep -q "$se" cities.csv; then
+				    grep "$se" cities.csv | awk -F ',' -v sc="$sc" '$8 == sc {print $4; exit}'
+			    else
+				    se="XX"
+				    echo "$se"
+			    fi
+		    fi
+		    ;;
+		    
